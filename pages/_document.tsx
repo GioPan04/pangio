b38b8945a49c057de8fb69e513b945dd25f72d6d
GioPan04 <gioele@pannetto.com>
2021-08-08 11:27:33 +0200

    Initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Head, Html, Main, NextScript } from 'next/document';
+import { DEFAULT_SEO } from '../seo.config';
+
+export default class AppDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <title key="title">{DEFAULT_SEO.title}</title>
+                    <meta
+                    key="description"
+                    name="description"
+                    content={DEFAULT_SEO.description}
+                    />
+                    <meta
+                    key="twitter:card"
+                    name="twitter:card"
+                    content={DEFAULT_SEO.twitter.cardType}
+                    />
+                    <meta
+                    key="twitter:site"
+                    name="twitter:site"
+                    content={DEFAULT_SEO.twitter.handle}
+                    />
+                    <meta
+                    key="og:url"
+                    property="og:url"
+                    content={DEFAULT_SEO.openGraph.url}
+                    />
+                    <meta
+                    key="og:type"
+                    property="og:type"
+                    content={DEFAULT_SEO.openGraph.type}
+                    />
+                    <meta
+                    key="og:title"
+                    property="og:title"
+                    content={DEFAULT_SEO.openGraph.title}
+                    />
+                    <meta
+                    key="og:description"
+                    property="og:description"
+                    content={DEFAULT_SEO.openGraph.description}
+                    />
+                    <meta
+                    key="og:image"
+                    property="og:image"
+                    content={DEFAULT_SEO.openGraph.image}
+                    />
+                    <meta
+                    key="og:image:width"
+                    property="og:image:width"
+                    content={DEFAULT_SEO.openGraph.imageWidth}
+                    />
+                    <meta
+                    key="og:image:height"
+                    property="og:image:height"
+                    content={DEFAULT_SEO.openGraph.imageHeight}
+                    />
+                    <meta
+                    key="og:locale"
+                    property="og:locale"
+                    content={DEFAULT_SEO.openGraph.locale}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
---
+++ seo.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    DEFAULT_SEO: {
+        title: 'Gioele Pannetto',
+        description: 'The official website of Gioele Pannetto',
+        openGraph: {
+          type: 'website',
+          locale: 'en_EN',
+          url: 'https://pangio.it',
+          title: 'Gioele Pannetto',
+          description: 'The official website of Gioele Pannetto',
+        //   image: '',
+          site_name: 'pangio.it',
+        //   imageWidth: 1200,
+        //   imageHeight: 1200
+        },
+        twitter: {
+          handle: '@giopan_sh',
+          cardType: 'summary_large_image'
+        }
+    }
+};
