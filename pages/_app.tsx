901a7c7ee9653a77df699d2936dbc7a1da6e5b64
Gioele Pannetto <gioele@pannetto.com>
2021-08-08 11:59:37 +0200

    Hello world!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  );
 }
-export default MyApp
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,67 +1,66 @@
 import React from 'react';
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { DEFAULT_SEO } from '../seo.config';
 
-export default class AppDocument extends Document {
+class AppDocument extends Document {
     render() {
         return (
             <Html lang="en">
                 <Head>
-                    <title key="title">{DEFAULT_SEO.title}</title>
                     <meta
-                    key="description"
-                    name="description"
-                    content={DEFAULT_SEO.description}
+                        key="description"
+                        name="description"
+                        content={DEFAULT_SEO.description}
                     />
                     <meta
-                    key="twitter:card"
-                    name="twitter:card"
-                    content={DEFAULT_SEO.twitter.cardType}
+                        key="twitter:card"
+                        name="twitter:card"
+                        content={DEFAULT_SEO.twitter.cardType}
                     />
                     <meta
-                    key="twitter:site"
-                    name="twitter:site"
-                    content={DEFAULT_SEO.twitter.handle}
+                        key="twitter:site"
+                        name="twitter:site"
+                        content={DEFAULT_SEO.twitter.handle}
                     />
                     <meta
-                    key="og:url"
-                    property="og:url"
-                    content={DEFAULT_SEO.openGraph.url}
+                        key="og:url"
+                        property="og:url"
+                        content={DEFAULT_SEO.openGraph.url}
                     />
                     <meta
-                    key="og:type"
-                    property="og:type"
-                    content={DEFAULT_SEO.openGraph.type}
+                        key="og:type"
+                        property="og:type"
+                        content={DEFAULT_SEO.openGraph.type}
                     />
                     <meta
-                    key="og:title"
-                    property="og:title"
-                    content={DEFAULT_SEO.openGraph.title}
+                        key="og:title"
+                        property="og:title"
+                        content={DEFAULT_SEO.openGraph.title}
                     />
                     <meta
-                    key="og:description"
-                    property="og:description"
-                    content={DEFAULT_SEO.openGraph.description}
+                        key="og:description"
+                        property="og:description"
+                        content={DEFAULT_SEO.openGraph.description}
+                    />
+                    {/* <meta
+                        key="og:image"
+                        property="og:image"
+                        content={DEFAULT_SEO.openGraph.image}
                     />
                     <meta
-                    key="og:image"
-                    property="og:image"
-                    content={DEFAULT_SEO.openGraph.image}
+                        key="og:image:width"
+                        property="og:image:width"
+                        content={DEFAULT_SEO.openGraph.imageWidth}
                     />
                     <meta
-                    key="og:image:width"
-                    property="og:image:width"
-                    content={DEFAULT_SEO.openGraph.imageWidth}
-                    />
+                        key="og:image:height"
+                        property="og:image:height"
+                        content={DEFAULT_SEO.openGraph.imageHeight}
+                    /> */}
                     <meta
-                    key="og:image:height"
-                    property="og:image:height"
-                    content={DEFAULT_SEO.openGraph.imageHeight}
-                    />
-                    <meta
-                    key="og:locale"
-                    property="og:locale"
-                    content={DEFAULT_SEO.openGraph.locale}
+                        key="og:locale"
+                        property="og:locale"
+                        content={DEFAULT_SEO.openGraph.locale}
                     />
                 </Head>
                 <body>
@@ -72,3 +71,5 @@ export default class AppDocument extends Document {
         );
     }
 }
+
+export default AppDocument;
