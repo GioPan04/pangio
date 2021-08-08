44c9151e935e3f4db2aadbef3df655e9e559c9e0
Gioele Pannetto <gioele@pannetto.com>
2021-08-08 14:42:12 +0200

    small ui fix for icons

--- components/ContactMeBar/index.tsx
+++ components/ContactMeBar/index.tsx
@@ -5,11 +5,11 @@ import { Codepen, GitHub, Instagram, Mail, Twitter } from 'react-feather';
 const ContactMeBar: React.FC = () => {
   return (
     <div className="flex flex-col fixed bottom-5 left-5 gap-3 vertical-separator">
-      <a href="https://github.com/GioPan04"><GitHub className="icon-hover-white" color="rgba(255, 255, 255, 0.5)" /></a>
-      <a href="mailto:gioele@pannetto.com"><Mail className="icon-hover-white" color="rgba(255, 255, 255, 0.5)" /></a>
-      <a href="https://codepen.io/giopan"><Codepen className="icon-hover-white" color="rgba(255, 255, 255, 0.5)" /></a>
-      <a href="https://twitter.com/giopan_sh"><Twitter className="icon-hover-white" color="rgba(255, 255, 255, 0.5)" /></a>
-      <a href="https://instagram.com/giopan.sh"><Instagram className="icon-hover-white" color="rgba(255, 255, 255, 0.5)" /></a>
+      <a href="https://github.com/GioPan04"><GitHub className="icon-hover-white" /></a>
+      <a href="mailto:gioele@pannetto.com"><Mail className="icon-hover-white" /></a>
+      <a href="https://codepen.io/giopan"><Codepen className="icon-hover-white" /></a>
+      <a href="https://twitter.com/giopan_sh"><Twitter className="icon-hover-white" /></a>
+      <a href="https://instagram.com/giopan.sh"><Instagram className="icon-hover-white" /></a>
     </div>
   );
 };
