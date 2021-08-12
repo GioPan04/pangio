f5d4b3065e754afdabdb0a260f212925dcd48441
Gioele Pannetto <gioele@pannetto.com>
2021-08-12 17:40:44 +0200

    fix: removed domain from image url (#9)

--- components/ProjectsSection/index.tsx
+++ components/ProjectsSection/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const ProjectsSection: React.FC = () => {
   return (
     <HomeSection className="flex flex-col gap-28" title="My projects" id="projects">
-      <Project name="Insegreto" imageUrl="http://localhost:3000/insegreto.png" tags={['frontend', 'backend']} link="https://insegreto.com">
+      <Project name="Insegreto" imageUrl="/insegreto.png" tags={['frontend', 'backend']} link="https://insegreto.com">
         In 2020 I started to collaborate with the owners of the social network to give a new life to the website.
         We redisigned the whole site. <br />
         The website was born in 2009 and today has more than 200.000 registred users with over 5 milions posts sent.
