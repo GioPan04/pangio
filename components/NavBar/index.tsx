867375ef4730b7fdc364bda7d6a5402642eb29ea
GioPan04 <gioele@pannetto.com>
2021-08-18 16:40:09 +0200

    New transition for menu

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu } from 'react-feather';
 import ILink from 'types/ILink';
 
 type NavBarTypes = {
@@ -7,13 +8,19 @@ type NavBarTypes = {
 };
 
 const NavBar: React.FC<NavBarTypes> = ({ show, links }) => {
+
+  const [ showMenu, setShowMenu ] = useState(false);
+
   return (
-    <nav className={'z-40 w-screen bg-accent bg-opacity-90 text-white p-2 fixed flex flex-row justify-center items-center transition-all duration-150 ' + (show ? 'top-0' : '-top-12')}>
-      <span className="text-2xl mx-2">{'{'}</span>
+    <nav className={'z-40 w-screen bg-accent bg-opacity-90 text-white p-2 fixed flex flex-row justify-center items-center transition-all duration-150 ' + (show ? 'top-0 ' : '-top-12 ') + (showMenu ? 'h-sc' : '')}>
+      {/* <span className="text-2xl mx-2">{'{'}</span>
       <div className="flex flex-row items-center gap-2 mt-1">
         {links.map((l, i) => <a href={l.link} key={i}>{`"${l.name}"`}{i < links.length - 1 ? ',' : ''}</a>)}
       </div>
-      <span className="text-2xl mx-2">{'}'}</span>
+      <span className="text-2xl mx-2">{'}'}</span> */}
+      <div className={'transition-opacity duration-300 ' + (showMenu ? 'opacity-0' : 'opacity-100')}>
+        <Menu onClick={() => setShowMenu(v => !v)} />
+      </div>
     </nav>
   );
 };
