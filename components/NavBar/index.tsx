15ae685614d03156aea6a85c2e2329e99cd0f29e
Gioele Pannetto <gioele@pannetto.com>
2021-08-20 15:18:19 +0200

    Responsive menu

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -13,16 +13,22 @@ const NavBar: React.FC<NavBarTypes> = ({ show, links }) => {
   const [ showMenu, setShowMenu ] = useState(false);
 
   return (
-    <nav className={'z-40 w-screen bg-accent bg-opacity-90 text-white p-2 fixed flex flex-row justify-center items-center transition-all duration-150 ' + (show ? 'top-0 ' : '-top-12 ') + (showMenu ? 'h-sc bg-opacity-100' : '')}>
-      {/* <span className="text-2xl mx-2">{'{'}</span>
-      <div className="flex flex-row items-center gap-2 mt-1">
-        {links.map((l, i) => <a href={l.link} key={i}>{`"${l.name}"`}{i < links.length - 1 ? ',' : ''}</a>)}
+    <nav className={'z-40 w-screen bg-accent bg-opacity-90 text-white sm:p-2 py-2 fixed transition-all duration-150 ' + (show ? 'top-0 ' : '-top-12 ') + (showMenu ? 'bg-opacity-100' : '')}>
+      <div className="hidden sm:flex flex-row justify-center items-center">
+        <span className="text-2xl mx-2">{'{'}</span>
+        <div className="flex flex-row items-center gap-2 mt-1">
+          {links.map((l, i) => <a href={l.link} key={i}>{`"${l.name}"`}{i < links.length - 1 ? ',' : ''}</a>)}
+        </div>
+        <span className="text-2xl mx-2">{'}'}</span>
       </div>
-      <span className="text-2xl mx-2">{'}'}</span> */}
-      <div className={'transition-opacity duration-100 ' + (showMenu ? 'opacity-0' : 'opacity-100')}>
+
+
+      <div className={'sm:hidden absolute transition-opacity duration-100 w-full px-2 flex flex-row justify-between ' + (showMenu ? 'opacity-0 ' : 'opacity-100 ')}>
+        <span className="font-title text-xl -mt-0.5">Gioele Pannetto</span>
         <Menu onClick={() => setShowMenu(v => !v)} />
       </div>
-      <div className={'relative w-full transition-all duration-500 py-3 ' + (showMenu ? 'h-screen opacity-100 visible' : 'h-10 opacity-0 invisible')}>
+
+      <div className={'relative w-full transition-all duration-500 p-3 ' + (showMenu ? 'h-screen opacity-100 visible' : 'h-0 opacity-0 invisible sm:hidden')}>
         <X onClick={() => setShowMenu(false)} className="absolute top-3 right-3 cursor-pointer" size={40} />
         <span className="text-2xl mt-2 mb-2 block font-title">Menu</span>
 
@@ -30,7 +36,7 @@ const NavBar: React.FC<NavBarTypes> = ({ show, links }) => {
           <span className="uppercase text-sm font-subtitle font-semibold mb-2 block text-opacity-90">Links</span>
           <div className="flex flex-col text-lg">
             <span>{'{'}</span>
-            {links.map((l, i) => <a className="ml-5" href={l.link} key={i}>{`"${l.name}"`}{i < links.length - 1 ? ',' : ''}</a>)}
+            {links.map((l, i) => <a className="ml-5" onClick={() => setShowMenu(false)} href={l.link} key={i}>{`"${l.name}"`}{i < links.length - 1 ? ',' : ''}</a>)}
             <span>{'}'}</span>
           </div>
         </div>
@@ -40,6 +46,7 @@ const NavBar: React.FC<NavBarTypes> = ({ show, links }) => {
             <div className="flex flex-row gap-4 mt-3"><Socials size={30} /></div>
         </div>
       </div>
+
     </nav>
   );
 };
