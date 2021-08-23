3e969082ed453a94abd9e1cbe6abf3e10dbef01c
GioPan04 <gioele@pannetto.com>
2021-08-23 13:08:31 +0200

    Format date blog post

--- components/PostTile/index.tsx
+++ components/PostTile/index.tsx
@@ -1,5 +1,6 @@
 import { IPost } from 'blog/octokit';
 import { PreviewRenderer } from 'common/PreviewRenderer';
+import { formatDistance } from 'date-fns';
 import marked from 'marked';
 import React from 'react';
 
@@ -8,10 +9,13 @@ type PostTileProps = {
 };
 
 const PostTile: React.FC<PostTileProps> = ({ data }) => {
+
+  const date = formatDistance(new Date(data.created_at), new Date(), {addSuffix: true});
+
   return (
     <article>
       <h3 className="font-title text-2xl">{data.title}</h3>
-      <span className="text-sm">{data.created_at} {data.comments} comments</span>
+      <span className="text-sm">{date} {data.comments} comments</span>
       {data.body && <p>{marked(data.body, {renderer: new PreviewRenderer()})}</p>}
     </article>
   );
