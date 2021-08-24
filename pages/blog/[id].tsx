8f501dce90a50ea53136b12f4c83874953a9103f
Gioele Pannetto <gioele@pannetto.com>
2021-08-24 14:47:47 +0200

    Render page

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -3,6 +3,7 @@ import octokit, { IPost } from 'blog/octokit';
 import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 import DefaultErrorPage from 'next/error';
+import marked from 'marked';
 
 type BlogPageProps = {
   post?: IPost;
@@ -11,12 +12,22 @@ type BlogPageProps = {
 const BlogPage: NextPage<BlogPageProps> = ({ post }) => {
 
   if(!post) return <DefaultErrorPage statusCode={404} />;
+
+  const rawMarked = () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const raw = marked(post.body!, {sanitize: true});
+    
+    return {
+      __html: raw
+    };
+  };
   
   return (
     <BasicPage>
       <div className="w-full flex justify-center">
         <span className="font-bold font-title text-secondary text-4xl">{post.title}</span>
       </div>
+        <div dangerouslySetInnerHTML={rawMarked()}></div>
     </BasicPage>
   );
 };
