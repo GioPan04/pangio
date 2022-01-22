d07e32ea3ef7e36396db554f9a3ab12831513771
GioPan04 <gioele@pannetto.com>
2022-01-22 20:25:40 +0100

    Added Easy Register project

--- components/ProjectsSection/index.tsx
+++ components/ProjectsSection/index.tsx
@@ -5,6 +5,11 @@ import React from 'react';
 const ProjectsSection: React.FC = () => {
   return (
     <HomeSection className="flex flex-col gap-28" title="My projects" id="projects">
+      <Project name="Easy Register" imageUrl="/easyregister.jpg" tags={['frontend', 'backend']} link="https://easyregister.it">
+        Currently in beta, Easy Register is a school register born to solve all the issues that most school register have this days.
+        One day I was tired of using a product that can barely work, so that same day I created this project. It&apos;s based on the newest, but stable,
+        technologies like NextJS and NodeJS. 
+      </Project>
       <Project name="Insegreto" imageUrl="/insegreto.png" tags={['frontend', 'backend']} link="https://insegreto.com">
         In 2020 I started to collaborate with the owners of the social network to give a new life to the website.
         We redisigned the whole site. <br />
